a8342742a4d70364fdce48757ed49e55127ea4d8
Arya Raj <aryaraj487567@gmail.com>
2026-07-23 02:03:59 +0530

    change index

--- init/index.js
+++ init/index.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const axios = require("axios");
+require("dotenv").config();
 const initData = require("./data.js");
 const Listing = require("../Models/listing.js");
 
@@ -9,14 +11,47 @@ main().then(()=>{
 })
 
 async function main(){
-    await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+    await mongoose.connect(process.env.ATLASDB_URL);
 }
 
-const initDB = async()=>{
-   await Listing.deleteMany({});
-   initData.data = initData.data.map((obj)=>({...obj, owner: "6a547ff7daa80ed56823d663"}));
-   await Listing.insertMany(initData.data);
-   console.log("Data was initialized");
-}
+const initDB = async () => {
+    await Listing.deleteMany({});
+
+    for (let listing of initData.data) {
+        try {
+            const response = await axios.get(
+                "https://nominatim.openstreetmap.org/search",
+                {
+                    params: {
+                        q: `${listing.location}, ${listing.country}`,
+                        format: "json",
+                        limit: 1,
+                    },
+                    headers: {
+                        "User-Agent": "wanderlust-app",
+                    },
+                }
+            );
+
+            if (response.data.length > 0) {
+                listing.geometry = {
+                    type: "Point",
+                    coordinates: [
+                        parseFloat(response.data[0].lon),
+                        parseFloat(response.data[0].lat),
+                    ],
+                };
+            }
+
+            listing.owner = "6a547ff7daa80ed56823d663";
+
+            await Listing.create(listing);
+        } catch (err) {
+            console.log(`Error for ${listing.title}:`, err.message);
+        }
+    }
+
+    console.log("Database initialized");
+};
 
 initDB();
